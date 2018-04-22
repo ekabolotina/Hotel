a78dbc664303e4e4f1cd6bb50609b3306f17fc91
Иван <ivan.rusia@mail.ru>
2018-04-22 23:25:14 +1000

    Make a layout of rooms

--- front/scripts/es/main.js
+++ front/scripts/es/main.js
@@ -2,9 +2,13 @@
 
     $(document).ready(() => {
 
-        new $JssorSlider$('slider1_container', {
-            $AutoPlay: true
-        });
+        $('.slider1_container').each((index, element) => {
+            const sliderId = 'slider1_container' + index;
+            $(element).attr('id', sliderId);
+            new $JssorSlider$(sliderId, {
+                $AutoPlay: true
+            });
+        })
 
     });
 
