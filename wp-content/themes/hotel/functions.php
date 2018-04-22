b18534205d13937c8bf5e8c143f91692db558e75
Иван <ivan.rusia@mail.ru>
2018-04-22 22:07:37 +1000

    Add menu & put it in header

--- wp-content/themes/hotel/functions.php
+++ wp-content/themes/hotel/functions.php
@@ -39,7 +39,7 @@ class StarterSite extends TimberSite {
 		$context['foo'] = 'bar';
 		$context['stuff'] = 'I am a value set in your functions.php file';
 		$context['notes'] = 'These values are available everytime you call Timber::get_context();';
-		$context['menu'] = new TimberMenu();
+		$context['menu'] = new Timber\Menu('header_menu');
 		$context['site'] = $this;
         $context['options'] = get_fields('option');
 		return $context;
